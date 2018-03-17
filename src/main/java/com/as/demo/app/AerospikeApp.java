e475f18a1a5e5153590a245718474c2e63c70767
Rajkumar Natarajan <rajkumar.natarajan@synacor.com>
2018-03-17 00:47:40 -0400

    updated for @Value test

--- src/main/java/com/as/demo/app/AerospikeApp.java
+++ src/main/java/com/as/demo/app/AerospikeApp.java
@@ -7,9 +7,25 @@ import com.as.demo.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class AerospikeApp {
 
-    public static void main(String []args) {
+    public static void main(String []args) throws Exception {
+        Properties props = new Properties();
+        ClassLoader classLoader = AerospikeApp.class.getClassLoader();
+        File file = new File(classLoader.getResource("local.properties").getFile());
+        props.load(new FileReader(file));
+       // System.out.println(File);
+        System.out.println("props.size() " +props.size());
+        props.forEach((k,v) -> {
+            System.out.println("k = "+k.toString()+" v ="+v.toString());
+            System.setProperty(k.toString(),v.toString());
+        });
         ApplicationContext appCtx = new AnnotationConfigApplicationContext(AerospikeConfig.class);
        // ApplicationContext appCtx =new ClassPathXmlApplicationContext("AerospikeConfig.xml");
         UserService us = appCtx.getBean(UserService.class);
--- src/main/java/com/as/demo/config/AerospikeConfig.java
+++ src/main/java/com/as/demo/config/AerospikeConfig.java
@@ -4,33 +4,66 @@ import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.policy.ClientPolicy;
 import com.as.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.data.aerospike.core.AerospikeTemplate;
 import org.springframework.data.aerospike.repository.config.EnableAerospikeRepositories;
+
+class AerospikeDataSourceCondition implements Condition {
+
+    @Value( "${userstore.host}")
+    private String host;
+
+    @Value( "${userstore.port}")
+    private int port;
+
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+
+        try {
+            System.out.println("host = "+ host);
+            System.out.println("port = "+ port);
+            System.out.println("${userstore.host} " + System.getProperty("userstore.host"));
+            System.out.println("${userstore.port} " + System.getProperty("userstore.port"));
+            new AerospikeClient(new ClientPolicy(), System.getProperty("userstore.host"), Integer.parseInt(System.getProperty("userstore.port")));
+            System.out.println("returns true");
+            return true;
+        } catch (Exception e) {
+
+        }
+        return false;
+    }
+}
+
 
 @Configuration
 @EnableAerospikeRepositories(basePackages = "com.as.demo.repositories")
 @ComponentScan(basePackageClasses = {UserService.class})
+@Conditional(value=AerospikeDataSourceCondition.class)
 public class AerospikeConfig {
 
+    @Value( "${userstore.host}")
+    private String host;
+
+    @Value( "${userstore.port}")
+    private int port;
+
+    @Value( "${userstore.test}")
+    private String namespace = "test";
+
     @Bean(destroyMethod = "close")
+    @Conditional(value=AerospikeDataSourceCondition.class)
     public AerospikeClient aerospikeClient() {
 
-        try {
-            ClientPolicy policy = new ClientPolicy();
-            policy.failIfNotConnected = true;
-            String localhost = "localhost";
-
-            return new AerospikeClient(policy, localhost, 3000);
-        } finally {
-            return null;
-        }
+        ClientPolicy policy = new ClientPolicy();
+        policy.failIfNotConnected = true;
+        return new AerospikeClient(policy, host, port);
 
     }
 
     @Bean
+    @Conditional(value=AerospikeDataSourceCondition.class)
     public  AerospikeTemplate aerospikeTemplate(@Autowired(required = false) AerospikeClient aerospikeClient) {
         if(aerospikeClient == null)
             return null;
